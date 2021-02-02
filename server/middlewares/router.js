4fa943a660424c45d90ffa06ded60e39e82a6a75
chenpan1@sfmail.sf-express.com <chenpan1@sfmail.sf-express.com>
2021-02-02 16:24:09 +0800

    delete

--- server/middlewares/router.js
+++
@@ -1,8 +0,0 @@
-const { Route } = require('../lib/decorator')
-const { resolve } = require('path')
-
-exports.router = async app => {
-  const apiPath = resolve(__dirname, '../routes')
-  const router = new Route(app, apiPath)
-  router.init()
-}
--- src/services/login.js
+++
@@ -1,9 +0,0 @@
-import { request } from 'umi';
-
-export async function fakeAccountLogin(params) {
-  return request('/api/admin/login', {
-    method: 'POST',
-    data: params,
-  });
-}
-
